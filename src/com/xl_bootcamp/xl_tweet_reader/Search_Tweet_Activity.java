a9832e974540da6972cbc43a6af17bb9c0c4b894
Paul Well <paulwells12@gmail.com>
2013-04-11 17:48:59 -0400

    edited layout of custom search screen program now cleans the search term of invalid characters before searching for it

--- src/com/xl_bootcamp/xl_tweet_reader/Search_Tweet_Activity.java
+++ src/com/xl_bootcamp/xl_tweet_reader/Search_Tweet_Activity.java
@@ -1,37 +1,14 @@
 package com.xl_bootcamp.xl_tweet_reader;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-
-
-
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class Search_Tweet_Activity extends Activity {
 	
@@ -68,7 +45,8 @@ public class Search_Tweet_Activity extends Activity {
 		protected Void doInBackground(Void... arg0){
 			EditText searchBox = (EditText) findViewById(R.id.search_box);
 			String searchPhrase = searchBox.getText().toString();
-			searchBox.clearFocus();
+			
+			searchPhrase = cleanHashTag(searchPhrase);
 			
 			//remove hashtag if user entered hashtag
 			if(searchPhrase.indexOf('#')==0){
@@ -85,6 +63,24 @@ public class Search_Tweet_Activity extends Activity {
 		
 	}
 	
+	//instead of throwing an error message just remove invalid characters from hashtag
+	//before searching
+	private String cleanHashTag(String tag){
+		//remove hashtag if user entered hashtag
+		if(tag.indexOf('#')==0){
+			tag = tag.substring(1);
+		}
+		
+		StringBuffer newBuffer = new StringBuffer(tag);
+		
+		for(int i = tag.length()-1; i >= 0;i--){
+			if(!Character.isLetterOrDigit(tag.charAt(i)) && !(tag.charAt(i) == '_'))
+				newBuffer.deleteCharAt(i);
+		}
+		
+		return newBuffer.toString();
+		
+	}
 	
 	public void searchForTweets(View v){
 		new NetworkCom().execute();
